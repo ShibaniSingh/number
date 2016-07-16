4918ef60da425fe49394d29ecf4a1a066b42734b
ShibaniSingh <shibani888ksriet@gmail.com>
2016-07-16 14:47:41 +0530

    Add files via upload

---
+++ FiveHello.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fivehello;
+
+/**
+ *
+ * @author Student
+ */
+public class FiveHello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s="hello";
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println(s);
+        }
+    }
+}
